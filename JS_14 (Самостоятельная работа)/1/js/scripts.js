20bbaaaa4aacc601ef0b170fd5ea323de4c478b7
Alex Sav <108679599+alexsav29@users.noreply.github.com>
2023-06-08 21:28:08 +0300

    Update_#1.js

--- JS_14 (Самостоятельная работа)/1/js/scripts.js
+++ JS_14 (Самостоятельная работа)/1/js/scripts.js
@@ -2,14 +2,23 @@ let img = document.querySelectorAll('.images img');
 
 img.forEach(function(element) {
     element.addEventListener('mouseover', function() {
-        this.style.backgroundColor('rgb(255, 255, 255, .1)');
-        this.style.cursor('pointer');
-        this.style.opacity('0.5');
+        this.style.backgroundColor = 'rgb(255, 255, 255, .1)';
+        this.style.cursor = 'pointer';
+        this.style.opacity = '1';
     
         console.log('Навели мышь на фото');
     });
 
-})
+});
+
+img.forEach(function(element) {
+    element.addEventListener('mouseout', function() {
+        this.style.opacity = '0.5';
+    
+        console.log('Убрали мышь с фото');
+    });
+
+});
 
 
 
